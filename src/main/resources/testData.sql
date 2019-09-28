5173f3ede5c6ded94ae94b4ababa960a1bd52eba
Sondre Bolland <sondrebolland@hotmail.com>
2019-09-28 15:29:26 +0200

    Added SQL script for dummy data

--- src/main/resources/testData.sql
+++ src/main/resources/testData.sql
@@ -1,1 +1,4 @@
+INSERT INTO G2019_01.IoTUser (ID, EMAIL, USERNAME, PASSWORD) VALUES (4, 'sondrebolland@hotmail.com', 'sonjerbolan', 'kake');
+INSERT INTO G2019_01.DEVICE (ID, PICTURE, URL, PUBLISHED, ONLINE) VALUES (0, 'Temp', 'google.com', 1, 1);
+INSERT INTO G2019_01.IoTUser_Device VALUES (0, 0);
 
