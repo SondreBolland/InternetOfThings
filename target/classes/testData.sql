5aa78d9f025ff1671c668bc53b0b6d31836c4e72
Sondre Bolland <sondrebolland@hotmail.com>
2019-09-28 11:49:26 +0200

    Done part 2. Added dummy data to the database via the persitance.xml with TestData.sql

---
+++ target/classes/testData.sql
@@ -0,0 +1,2 @@
+INSERT INTO G2019_01.IoTUser (ID, EMAIL, USERNAME, PASSWORD) VALUES (1, 'larsvågenes@hotmail.com', 'lars', 'ball')
+INSERT INTO G2019_01.IoTUser (ID, EMAIL, USERNAME, PASSWORD) VALUES (2, 'alf@hotmail.com', 'alf', 'kake')
