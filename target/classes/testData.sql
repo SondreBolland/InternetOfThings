7ba58a8b9bfb1f698d9782c7f1e7f3e7b401cc95
Sondre Bolland <sondrebolland@hotmail.com>
2019-09-30 15:11:46 +0200

    Added dummy data for Register table

--- target/classes/testData.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO G2019_01.IoTUser (ID, EMAIL, USERNAME, PASSWORD) VALUES (9, 'sondrebolland@hotmail.com', 'sonjerbolan', 'kake')
-INSERT INTO G2019_01.DEVICE (ID, PICTURE, URL, PUBLISHED, ONLINE) VALUES (9, 'Temp', 'google.com', 1, 1)
-INSERT INTO G2019_01.IoTUser_Device VALUES (9, 0)
-
